db6b7fd2fec5ef23bd90e64cc58d02f02e8603c5
Northwemko <yinyangxiangsheng@gmail.com>
2021-11-27 00:18:48 +0800

    up

--- src/perturbation.jl
+++ src/perturbation.jl
@@ -3,7 +3,7 @@ function diagonal(op::Operator{T,S}) where {T,S}
     (op.ks == op.bs && op.c ≈ op.c') || error("Operator maxtrix is not hermitian")
     m = op.c
     h = Hermitian(m)
-    values, vectors = eigen(h)
+    values, vectors = eigen(h)  # default sortby values (ascending)
     ket_vectors = Vector{KetVec{float(T),S}}(undef, size(op, 2))
     for i = 1:size(op, 1)
         vec = vectors[1, i] >= 0 ? vectors[:, i] : -vectors[:, i]
